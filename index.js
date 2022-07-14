b5fd0c2ea9848423780dc292aed6e6279ff020cb
bryant fowler <fowler1221bryant@gmail.com>
2022-07-14 16:28:50 -0400

    generateMarkdown

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-const { createDecipheriv } = require("crypto");
 
-const promptUser = () => {};
-return inquirer.prompt([
-  {
-    type: "input",
-    name: "projectTitle",
-    message: "What is the title of your project?: ",
-  },
-  {
-    type: "input",
-    name: "projectDescription",
-    message: "Describe your project. (Required): ",
-  },
-  {
-    type: "input",
-    name: "installationInstructions",
-    message: "What are the instructionsto install your application?:\n\n",
-  },
-  {
-    type: "input",
-    name: "contributionInstructoins",
-    message:
-      "What are the contributors contribution install your application?:\n\n",
-  },
-  {
-    type: "list",
-    name: "projectLicense",
-    message: "Which license applies",
-    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-  },
-  {
-    type: "input",
-    name: "usageInstructions",
-    message: "What are the instruction to use your application",
-  },
-  {
-    type: "input",
-    name: "githubUsername",
-    message: "What is your github?:",
-  },
-  {
-    type: "input",
-    name: "emailAddress",
-    message: "What is your email address?:",
-  },
-]);
-
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is the title of your project?: ",
+    },
+    {
+      type: "input",
+      name: "projectDescription",
+      message: "Describe your project. (Required): ",
+    },
+    {
+      type: "input",
+      name: "installationInstructions",
+      message: "What are the instructionsto install your application?:\n\n",
+    },
+    {
+      type: "input",
+      name: "contributionInstructoins",
+      message:
+        "What are the contributors contribution install your application?:\n\n",
+    },
+    {
+      type: "list",
+      name: "projectLicense",
+      message: "Which license applies",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+    {
+      type: "input",
+      name: "usageInstructions",
+      message: "What are the instruction to use your application",
+    },
+    {
+      type: "input",
+      name: "githubUsername",
+      message: "What is your github?:",
+    },
+    {
+      type: "input",
+      name: "emailAddress",
+      message: "What is your email address?:",
+    },
+  ]);
+};
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
@@ -58,8 +57,8 @@ function writeToFile(fileName, data) {
 
 const init = async () => {
   let userInput = await promptUser();
-  let markdownText = generateMarkdown(UserInput);
-  writeToFile("README.md", markdownText);
+  let markdownText = generateMarkdown(userInput);
+  writeToFile("./output/README.md", markdownText);
 };
 
 // Function call to initalize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return `! [badge-image](https://img.shields.io/static/v1?label=license&message=${license.replace(
+      "",
+      "_"
+    )})&color=green)`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return `https://choosealicense.com/licenses/${license
+      .toLowerCase()
+      .replace("", "_")}/`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    licenseLink = renderLicenseLink(license);
+    return `## License`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let licensebadge = renderLicenseBadge(data.projectTitle);
+  let renderLicenseSection = renderLicenseSection(data.projectTitle);
+  return `# ${data.projectTitle}
+  ${licensebadge}
 
-`;
+  ## Table of Contents
+
+  -- [Description](#description)
+
+  -- [Installation](#installation)
+
+  -- [Usage](#usage)
+
+  -- [Testing](#testing)
+
+  -- [Contribute](#contribute)
+
+  -- [License](#license)
+
+  -- [Questions](#license)
+
+  ## Description
+  ${data.projectDescription}
+
+  ## Installation
+  ${installationInstructions}
+  
+  ## Usage
+  ${usageInstruction}
+
+  ## Testing
+  ${testInstructions}
+
+  ## Contribute
+  ${data.contributionInstructions}
+
+  ${licenseSection}
+
+  ## Questions?
+  Any questions or concerns with project please email me or contact via github.
+
+  Github: https://github.com/${data.githubUser.name}
+
+  Email: ${data.emailAddress}
+  `;
 }
-
 module.exports = generateMarkdown;
